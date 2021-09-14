9e6236688e3e8fea0688d06aa3e48ecb59da5662
Huy Quang <huyquang271099@gmail.com>
2021-09-15 05:22:35 +0700

    findAll category

--- src/main/java/com/libra/web/controller/admin/CategoriesController.java
+++ src/main/java/com/libra/web/controller/admin/CategoriesController.java
@@ -1,20 +1,29 @@
 package com.libra.web.controller.admin;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.libra.core.entities.Category;
 import com.libra.core.services.ICategoriesService;
 
 @Controller
+@RequestMapping("/admin/category")
 public class CategoriesController {
 	@Autowired
 	private ICategoriesService categoriesService;
 	
 	// category form
+	@GetMapping
 	public String getAll(Model model) {
+		List<Category> categoryList = categoriesService.findAll();
 		
-		model.addAttribute("title", "Thể Loại Sácg");
-		return "";
+		model.addAttribute("category", categoryList);
+		model.addAttribute("title", "Thể Loại Sách");
+		return "admin/categories/categoryPage";
 	}
 }
