883ff6d70a9c788f675d65de3cc35ce4239ae95a
Huy Quang <huyquang271099@gmail.com>
2021-09-16 12:30:41 +0700

    Code gửi mail

--- src/main/java/com/libra/web/controller/ForgotPasswordController.java
+++ src/main/java/com/libra/web/controller/ForgotPasswordController.java
@@ -1,9 +1,15 @@
 package com.libra.web.controller;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +27,8 @@ public class ForgotPasswordController {
 	
 	@Autowired
 	private IUserService userService;
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	@GetMapping("/forgot_password")
 	public String showForgotPasswordForm(Model model) {
@@ -43,10 +51,43 @@ public class ForgotPasswordController {
 			String resetPasswordLink = Utility.getSiteURL(request) + "/reset_password?token=" + token;
 			System.out.println(resetPasswordLink);
 			
+			// thuc hien gui mail
+			try {
+				sendMail(email, resetPasswordLink);
+				session.setAttribute("message", new MessageResponse("Chúng tôi đã gửi mã xác nhận đến gmail của bạn! Vui lòng kiểm tra", "alert-success"));
+			} catch (UnsupportedEncodingException | MessagingException e) {
+				e.printStackTrace();
+				session.setAttribute("message", new MessageResponse("Có lỗi khi thực hiện!!", "alert-danger"));
+			}
+			
 		} catch (UsernameNotFoundException e) {
 			session.setAttribute("message", new MessageResponse(e.getMessage(), "alert-error"));
 		}
 		
 		return "forgot_password_form";
+	}
+	// gui mail
+	private void sendMail(String email, String resetPasswordLink) throws UnsupportedEncodingException, MessagingException {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		helper.setFrom("contact@librarybook.com", "LibraryManager Support");
+		helper.setTo(email);
+		
+		String subject = "đường link để reset password của bạn";
+	     
+	    String content = "<p>Xin chào, </p>"
+	            + "<p>Bạn đã yêu cầu đặt lại mật khẩu của mình. </p>"
+	            + "<p>Nhấp vào liên kết bên dưới để thay đổi mật khẩu của bạn: </p>"
+	            + "<p><a href=\"" + resetPasswordLink + "\">Thay đổi mật khẩu của tôi </a></p>"
+	            + "<br>"
+	            + "<p>Bỏ qua email này nếu bạn nhớ mật khẩu của mình, "
+	            + "hoặc bạn đã không thực hiện yêu cầu.</p>";
+	     
+	    helper.setSubject(subject);
+	     
+	    helper.setText(content, true);
+	     
+	    mailSender.send(message);
 	}
 }
